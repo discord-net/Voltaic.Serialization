2a7b27154790b1e4165614a41efe2f23e9ce9a37
RogueException <rogueexception@gmail.com>
2018-07-26 23:00:31 -0700

    Split from Wumpus.Net repo

---
+++ src/Voltaic.Serialization.Json/JsonTokenType.cs
@@ -0,0 +1,19 @@
+﻿namespace Voltaic.Serialization.Json
+{
+    public enum JsonTokenType : byte
+    {
+        None = 0,
+        Null,
+        Undefined,
+        True,
+        False,
+        Number,
+        String,
+        StartObject,
+        EndObject,
+        StartArray,
+        EndArray,
+        ListSeparator,
+        KeyValueSeparator
+    }
+}
